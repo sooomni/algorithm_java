27ab157719b511bb10853c352f2bb510b922bf3f
김수민 <sooom@Sooms-MacBookPro.local>
2021-03-02 00:00:41 +0900

    update b1759

--- Main_b1759_암호만들기.java
+++ Main_b1759_암호만들기.java
@@ -1,74 +1,51 @@
 import java.io.*;
 import java.util.*;
 
-public class Main2 {
+public class Main_b1759_암호만들기 {
 
-	private static char [] input;
-	private static char [] arr;
-	private static List <char []> result;
+	private static String[] input;
 	private static int l,c;
+	
 	public static void main(String [] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		l = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());	
 		
-		input = new char[c];
-		arr = new char[c];
-		result = new ArrayList<char []>();
+		input = new String[c];
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0;i<c;i++) {
-			input[i] = st.nextToken().charAt(0);
-		}
+		input = br.readLine().split(" ");
+		Arrays.sort(input);
+		
+		combination(0,"");
 
-		combination(l,0,0);
-		
-		Collections.sort(result, new Comparator<char []>() {
-
-			@Override
-			public int compare(char[] o1, char[] o2) {
-				Arrays.sort(o1);
-				Arrays.sort(o2);
-				
-				return String.valueOf(o1).compareTo(String.valueOf(o2));
-			}
-			
-		});
-		
-		for(char [] tmp: result) {
-			for(char c :tmp) {
-				System.out.print(c);
-			}
-			System.out.println();
-		}
-		
 	}
 	
-	private static void combination(int m, int target, int index){
-		if(m == 0) {
-			if(check(arr)) {
-				char [] tmp = arr.clone();
-				result.add(tmp);
+	private static void combination(int index, String ref){
+		if(ref.length() == l) {
+			if(check(ref)) {
+				System.out.println(ref);
 			}
 			return;
 		}
 		
-		if(target == c) return;
+		if(index >= c) return;
 		
-		arr[index] = input[target];
-		combination(m-1,target+1,index+1);
-		combination(m,target+1,index);
+		combination(index+1,ref+input[index]);
+		combination(index+1,ref);
 		
 	}
 	
-	private static boolean check(char [] arr) {
+	private static boolean check(String arr) {
 		int cnt = 0, cnt2 = 0;
-		for(char c : arr){
-			if(c == 'a' || c == 'e'|| c == 'i'||c == 'o' || c == 'u') cnt++;
-			else cnt2++;
+		for(int i = 0;i<arr.length();i++){
+			 if(isVowel(arr.charAt(i))) cnt++;
+			 else cnt2++;
 		}
 		return cnt >= 1 && cnt2 >= 2 ? true :false;
 
+	}
+	private static boolean isVowel(char c) {
+		return (c == 'a' || c == 'e'|| c == 'i'||c == 'o' || c == 'u') ? true : false;
 	}
 }
