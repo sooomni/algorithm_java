e66a727d8f8eef9be28aceed2ec5b5dabe176639
Soomin Kim <soomin0403@naver.com>
2021-05-22 00:20:14 +0900

    modify b16968

--- Main_b16968_차량번호판1.java
+++ Main_b16968_차량번호판1.java
@@ -5,44 +5,28 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_b16968_차량번호판1 {
-	private static boolean [] alpha = new boolean[26];
-	private static boolean [] number = new boolean[10];
 	private static char [] input;
 	private static int cnt = 0;
 	public static void main(String [] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		input = st.nextToken().toCharArray();
-		dfs(0);
+		for(int i = 0;i<input.length;i++) {
+			if(input[i] == 'c') {
+				if(cnt == 0) cnt += 26;
+				else {
+					if(input[i-1] == 'c') cnt *= 25;
+					else cnt *= 26;
+				}
+			}
+			else {
+				if(cnt == 0) cnt += 10;
+				else {
+					if(input[i-1] == 'd') cnt *= 9;
+					else cnt *= 10;
+				}
+			}
+		}
 		System.out.println(cnt);
-	}
-	public static void dfs(int cur){
-		
-		if(cur == input.length) {
-			cnt+=1;
-			return;
-		}
-
-		if(input[cur] == 'd') {
-			if(cur > 1 && input[cur-1] != 'd') Arrays.fill(number, false);
-			for(int i = 0;i<10;i++) {
-				if(!number[i]) {
-					number[i] = true;
-					dfs(cur+1);
-				}
-				number[i] = false;
-			}
-		}
-		else {
-			if(cur > 1 && input[cur-1] != 'c') Arrays.fill(alpha, false);
-			
-			for(int i = 0;i<26;i++) {
-				if(!alpha[i]) {
-					alpha[i] = true;
-					dfs(cur+1);
-				}
-				alpha[i] = false;
-			}
-		}
 	}
 }
